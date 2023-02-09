62d972690cf304b829f134ce6531f43cb90d74f2
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-02-09 15:55:27 -0500

    Finished coding the scanner.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Working through part 2 of crafting interpreters.
+
+[Section Link](https://craftinginterpreters.com/a-tree-walk-interpreter.html)
--- src/main/java/com/thanazer/lox/Lox.java
+++ src/main/java/com/thanazer/lox/Lox.java
@@ -6,8 +6,11 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
+  static boolean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -22,6 +25,7 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+    if (hadError) System.exit(65);
   }
 
   private static void runPrompt() throws IOException {
@@ -33,10 +37,27 @@ public class Lox {
       String line = reader.readLine();
       if (line == null) break;
       run(line);
+      hadError = false;
     }
   }
 
   private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
 
+    for (Token token: tokens) {
+      System.out.println(token);
+    }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println(
+      "[line " + line + "] Error" + where + ": " + message
+    );
+    hadError = true;
   }
 }

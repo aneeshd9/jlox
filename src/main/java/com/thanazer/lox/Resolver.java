bbeeafce14d5856729f22154a8f2cf8a87ee4713
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-02-18 10:10:43 -0500

    Added methods on classes.

--- src/main/java/com/thanazer/lox/Resolver.java
+++ src/main/java/com/thanazer/lox/Resolver.java
@@ -22,7 +22,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum FunctionType {
     NONE,
-    FUNCTION
+    FUNCTION,
+    METHOD
   }
 
   void resolve(List<Stmt> statements) {
@@ -59,6 +60,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitClassStmt(Class stmt) {
     declare(stmt.name);
     define(stmt.name);
+
+    for (Stmt.Function method: stmt.methods) {
+      FunctionType declaration = FunctionType.METHOD;
+      resolveFunction(method, declaration);
+    }
     return null;
   }
 
